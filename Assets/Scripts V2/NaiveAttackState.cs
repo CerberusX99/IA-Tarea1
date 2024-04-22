18a28ac0a0f365291767964f84aa7492252653c1
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2024-04-22 12:31:50 -0600

    V.08

---
+++ Assets/Scripts V2/NaiveAttackState.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class NaiveAttackState : NaiveFSMState
+{
+    private PatrolAgentFSM PatrolFSMRef = null;
+    //Variable para tener control del rango de distancia con el jugador
+    public GameObject agent;
+
+    //Variables que manejan la distancia y angulo de vision
+    public float AttackVisionAngleMultiplier;
+    public float AttackVisionDistanceMultiplier;
+    //Variables para regresar a otro estado
+    public float TimeToChangeState;
+    public float TimeBeforeChangeState = 0f;
+
+
+    public NaiveAttackState(NaiveFSM FSM)
+    {
+        Name = "Attack";
+        _FSM = FSM;
+        PatrolFSMRef = ((PatrolAgentFSM)_FSM);
+        
+    }
+
+    //Inicializamos la distancia y angulo de vision de este estado en la maquina de estados para que cambie
+    public void Init(float in_AttackVisionAngleMultiplier, float in_AttackVisionDistanceMultiplier)
+    {
+        AttackVisionAngleMultiplier = in_AttackVisionAngleMultiplier;
+        AttackVisionDistanceMultiplier = in_AttackVisionDistanceMultiplier;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        //Inicializamos la variable de cuanto tiempo tardara en perder el estado de ataque
+        TimeToChangeState = 5f;
+        //Inicializamos la animacion al entrar a este estado
+        PatrolFSMRef._Animator.SetBool("Ataque", true);
+        
+        PatrolFSMRef.PlayAttackMusic(); 
+        //PatrolFSMRef._Animator.SetBool("Alerta", true);
+        agent = GameObject.Find("Ghost");
+        PatrolFSMRef._light.color = Color.red;
+    }
+
+    public override void Update()
+    {
+        PatrolFSMRef.Shoot();
+        base.Update();
+        //Establecemos para donde se va a dirigir mi personaje en el estado de ataque
+        Vector3 directionToPlayer = agent.transform.position - _FSM.transform.position;
+        //Establecemos la luz roja
+        PatrolFSMRef._light.color = Color.red;
+        //Aplicamos el navmesh cuando se vaya en direccion a mi jugador
+        PatrolFSMRef._NavMeshAgent.SetDestination(agent.transform.position);
+
+        //Empezamos a hacer que corra el timmpo para regresar de estado
+        TimeToChangeState -= Time.deltaTime;
+
+        //Si se acaba el tiempo regresa al estado de alerta
+        if (TimeToChangeState <= TimeBeforeChangeState)
+        {
+            NaivePatrolState AlertStateInstance = PatrolFSMRef.PatrolStateRef;
+            _FSM.ChangeState(AlertStateInstance);
+            return;
+        }
+       
+        //Si la distancia de la direccion a la que vamos con el jugador es menor a una unidad de unity destruye el personaje
+        if (directionToPlayer.magnitude < 1.0f)
+        {
+            NaivePatrolState PatrolStateInstance = PatrolFSMRef.PatrolStateRef;
+            _FSM.ChangeState(PatrolStateInstance);
+            DestroyPlayer();
+            return;
+        }
+
+    }
+
+    public override void Exit()
+    {
+        //Salimos de las animacioes
+        PatrolFSMRef._Animator.SetBool("Ataque", false);
+        PatrolFSMRef._Animator.SetBool("Alerta", true);
+        base.Exit();
+
+    }
+
+    private void DestroyPlayer()
+    {
+        //Apagas a mi personaje
+        agent.SetActive(false);
+        Debug.Log("El jugador ha sido destruido.");
+    }
+}
--- Assets/Scripts/TPM.cs
+++ Assets/Scripts/TPM.cs
@@ -1,119 +1,115 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AgentController : MonoBehaviour
+public class ThirdPersonMovement : MonoBehaviour
 {
-    public CharacterController ControladorDeAgente;
-    public float velocidadDeMovimiento = 5f;
-    public float Gravedad = 9.8f;
-    public float fallVelocity;
-    public float FuerzaDeSalto=0f;
-    public float VelocidadDeRotacion = 20f;
-    public bool Agachado=false;
+    private CharacterController characterController;
+    public Transform camera;
+    public float speed = 4;
+    public float runSpeed = 8;
+    private float gravity = -9.8f;
 
+    // Declaro el animator
+    private Animator animator;
+    public AudioSource footstepAudio;
+    private bool isCrouching = false;
+    private bool isWalkingCrouched = false;
 
-    private Vector3 movePlayer;
-    public Camera cam;
-    private Vector3 camForward;
-    private Vector3 camRigth;
-
-    Animator animator;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        animator = GetComponent<Animator>();   
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float MovimientoHorizontalX = Input.GetAxis("Horizontal");
-        float MovimientoHorizontalZ = Input.GetAxis("Vertical");
-        Vector3 DireccionDeMovimiento = new Vector3(MovimientoHorizontalX, 0, MovimientoHorizontalZ);
-        DireccionDeMovimiento.Normalize();
+        // Defino que si el personaje no se esta moviendo su booleano sea falso y asi se inicie la animacion de idle
+        animator.SetBool("Walking", false);
 
-        camDirection();
+        // Input para el movimiento horizontal y vertical
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 movement = Vector3.zero;
+        float movementSpeed = 0;
 
-        movePlayer = DireccionDeMovimiento.x * camRigth + DireccionDeMovimiento.z * camForward;
-
-        movePlayer = movePlayer * velocidadDeMovimiento;
-
-
-        //MOVIMIENTO
-        if (DireccionDeMovimiento.magnitude <= 0.1)
+        // Calculo la direccion del movimiento
+        if (hor != 0 || ver != 0)
         {
-            animator.SetBool("Walking", false);
+            Vector3 forward = camera.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = camera.right;
+            right.y = 0;
+            right.Normalize();
+
+            Vector3 direction = forward * ver + right * hor;
+            movementSpeed = Mathf.Clamp01(direction.magnitude);
+            direction.Normalize();
+
+            // Determino la velocidad de movimiento
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            {
+                movement = direction * runSpeed * movementSpeed * Time.deltaTime;
+                animator.SetBool("Walking", false);
+                animator.SetBool("Run", true);
+            }
+            else
+            {
+                movement = direction * speed * movementSpeed * Time.deltaTime;
+                animator.SetBool("Walking", true);
+                animator.SetBool("Run", false);
+            }
+
+            // Aplico la rotacion
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.2f);
+
+            // Reproduzco el sonido de los pasos
+            if (!footstepAudio.isPlaying)
+            {
+                footstepAudio.Play();
+                footstepAudio.loop = true;
+            }
         }
         else
         {
-            animator.SetBool("Walking", true);
+            footstepAudio.Stop();
+            footstepAudio.loop = false;
         }
-    
 
+        // Agrego la gravedad
+        movement.y += gravity * Time.deltaTime;
+        // Aplico el movimiento
+        characterController.Move(movement);
 
-        ControladorDeAgente.transform.LookAt(ControladorDeAgente.transform.position + movePlayer);
-
-        SetGravity();
-
-        PlayerSkills();
-
-        ControladorDeAgente.Move(movePlayer * Time.deltaTime);
-        
-    }
-
-    private void camDirection()
-    {
-        camForward = cam.transform.forward;
-        camRigth = cam.transform.right;
-        camForward.y = 0;
-        camRigth.y = 0;
-
-        camForward = camForward.normalized;
-        camRigth = camRigth.normalized;
-    }
-
-    private void SetGravity()
-    {
-        if(ControladorDeAgente.isGrounded )
+        // Crouching input handling
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            fallVelocity = -Gravedad * Time.deltaTime;
-            movePlayer.y = fallVelocity;
+            isCrouching = !isCrouching;
+            animator.SetBool("Crouching", isCrouching);
+        }
+
+        // Jumping input handling
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jumping");
+           
+            movement.y = Mathf.Sqrt(-2 * gravity * 1.5f);
+             animator.SetBool("Jump", false);
+        }
+
+        // Walking while crouched input handling
+        if (isCrouching && Input.GetKey(KeyCode.W))
+        {
+            isWalkingCrouched = true;
+            animator.SetBool("WalkingC", true);
         }
         else
         {
-            fallVelocity -= Gravedad *Time.deltaTime;
-            movePlayer.y = fallVelocity;
+            isWalkingCrouched = false;
+            animator.SetBool("WalkingC", false);
         }
-        animator.SetBool("IsGrounded",ControladorDeAgente.isGrounded);    
     }
-
-    private void PlayerSkills()
-    {
-        if(ControladorDeAgente.isGrounded && Input.GetButtonDown("Jump") && Agachado==false)
-        {
-            fallVelocity = FuerzaDeSalto;
-            movePlayer.y = fallVelocity;
-            animator.SetTrigger("Jump");
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftControl) && Agachado == false)
-        {
-            animator.SetTrigger("Crouch");
-            Agachado = true;
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftControl) && Agachado == true)
-        {
-            animator.SetTrigger("Crouch");
-            Agachado = false;
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            animator.SetTrigger("Attack");
-        }
-      
-    }
-
 }
